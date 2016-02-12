756958a6385a99228cc07e8d915a58c51849b8d7
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-12 14:31:06 -0500

    Update play.c

--- play.c
+++ play.c
@@ -1,5 +1,5 @@
 #include "stdio.h"
-void play(float secs,int freq,int vol)
+void play(float secs,float freq,int vol)
 {
     int pulse=secs*8000.0; //amount of an 8000th of a second pulse
     int mod=8000.0/freq;
