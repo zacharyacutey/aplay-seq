0f83f61b7480480c3dad6124a611afe145dff896
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-12 17:24:16 -0500

    Update play.c

--- play.c
+++ play.c
@@ -15,51 +15,5 @@ void play(float secs,float freq)
 }
 int main()
 {
-    float d=BEAT*3/2;
-    float q=BEAT;
-    float e=BEAT/2;
-    float h=BEAT*2;
-    int i;
-    while(1)
-    {
-    play(d,329.628);
-    play(e,415.305);
-    play(q,493.883);
-    play(e,415.305);
-    play(e,329.628);
-    play(d,440.000);
-    play(e,523.251);
-    play(q,659.255);
-    play(e,587.330);
-    play(e,523.251);
-    play(d,493.883);
-    play(e,523.251);
-    play(q,587.330);
-    play(q,659.255);
-    play(q,523.251);
-    play(q,440.000);
-    play(h,440.000);
-    i = 2;
-    while(i!=0)
-    {
-      play(d,698.456);
-      play(e,783.991);
-      play(q,880.000);
-      play(e,783.991);
-      play(e,698.456);
-      play(d,659.255);
-      play(e,698.456);
-      play(q,659.255);
-      play(e,587.330);
-      play(e,523.251);
-      play(d,493.883);
-      play(e,523.251);
-      play(q,587.330);
-      play(q,659.255);
-      play(q,523.251);
-      play(q,440.000);
-      play(h,440.000);
-      i--;
-    }
-    }
+    
 }
