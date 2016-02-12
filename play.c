5364771212b313d26a3fea74123a6a5f2fd93e76
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-12 14:27:36 -0500

    Update play.c

--- play.c
+++ play.c
@@ -1,1 +1,12 @@
-
+#include "stdio.h"
+void play(float secs,int freq,int vol)
+{
+    int pulse=secs*8000.0; //amount of an 8000th of a second pulse
+    int mod=8000.0/freq;
+    int i=0;
+    while(i!=pulse)
+    {
+      putchar(char((i%mod==0)*vol));
+      i++;
+    }
+}
