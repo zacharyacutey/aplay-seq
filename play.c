1dc3ad8d6558bfcb632ac71d6307a9b76aba625f
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-14 16:24:53 -0500

    Update play.c

--- play.c
+++ play.c
@@ -5,7 +5,7 @@ void play(float secs,float freq)
 {
     int vol=VOL;
     int pulse=secs*8000.0; //amount of an 8000th of a second pulse
-    int mod=8000.0/freq*2;
+    int mod=8000.0/freq;
     int i=0;
     while(i!=pulse)
     {
